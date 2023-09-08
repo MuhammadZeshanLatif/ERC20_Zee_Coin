189b89dfb02f704498184fc59060b9ef38547915
Muhammad Zeeshan Latif <110631680+MuhammadZeshanLatif@users.noreply.github.com>
2023-09-09 01:26:53 +0500

    Erc

--- Zee_Token/contracts/ZeeToken.sol
+++ Zee_Token/contracts/ZeeToken.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
- contract ZeeToken is ERC20{
-     constructor(uint initialSupply) ERC20("ZeeToken","ZEE"){
-    _mint(msg.sender, initialSupply);
-     }
- }
+contract ZeeToken is ERC20Capped {
+    address public owner;
+
+    constructor(uint256 cap) ERC20("ZeeToken", "ZEE") ERC20Capped(cap *(10**decimals())){
+        owner = msg.sender;
+        _mint(owner, 70000000 * 10**decimals());
+    }
+}
